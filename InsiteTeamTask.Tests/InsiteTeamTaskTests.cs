c96c1a09940df6a6de1cbf830de5b531121ea86a
Thomas Clackson <thomas.clackson@icloud.com>
2020-10-08 10:39:50 +0100

    Added overloads for GetAttendanceListFor using gameNumber + seasonNumber and productCode

--- InsiteTeamTask.Tests/InsiteTeamTaskTests.cs
+++ InsiteTeamTask.Tests/InsiteTeamTaskTests.cs
@@ -1,5 +1,7 @@
 using InsiteTeamTask.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace InsiteTeamTask.Tests
@@ -8,7 +10,7 @@ namespace InsiteTeamTask.Tests
     public class InsiteTeamTaskTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Assert_GetAttendanceListFor_gameNumber_Returns_Data()
         {
             // Arrange
             var mockDataService = new MockDataService();
@@ -21,6 +23,81 @@ namespace InsiteTeamTask.Tests
 
             // Assert
             Assert.IsNotNull(attendanceList);
+        }
+
+        [TestMethod]
+        public void Assert_GetAttendanceListFor_gameNumber_Returns_Correctly()
+        {
+            // Arrange
+            var mockDataService = new MockDataService();
+            var repo = new DataRepository();
+
+            var expectedBarcodes = new List<string>
+            {
+                "969312288",
+                "946423612",
+                "969341288",
+                "969345292"
+            };
+
+            // Act
+            var attendanceList = repo.GetAttendanceListFor(3);
+
+            // Assert
+            var barcodes = attendanceList.Select(attendance => attendance.Barcode).ToList();
+            CollectionAssert.AreEqual(barcodes, expectedBarcodes);
+        }
+
+        [TestMethod]
+        public void Assert_GetAttendanceListFor_gameNumber_seasonNumber_Returns_Correctly()
+        {
+            // Arrange
+            var mockDataService = new MockDataService();
+            var repo = new DataRepository();
+
+            var expectedBarcodes = new List<string>
+            {
+                "946423612"
+            };
+
+            // Act
+            var attendanceList = repo.GetAttendanceListFor(3, 19);
+
+            // Assert
+            var barcodes = attendanceList.Select(attendance => attendance.Barcode).ToList();
+            CollectionAssert.AreEqual(barcodes, expectedBarcodes);
+        }
+
+
+        [TestMethod]
+        public void Assert_GetAttendanceListFor_productCode_Returns_Correctly()
+        {
+            // Arrange
+            var mockDataService = new MockDataService();
+            var repo = new DataRepository();
+
+            var expectedBarcodes = new List<string>
+            {
+                "964412136",
+                "934414901",
+                "966223434",
+                "962421512",
+                "964422511",
+                "916394028",
+                "901970287",
+                "926340037",
+                "912133969",
+                "901223783",
+                "951434130",
+                "923093162"
+            };
+
+            // Act
+            var attendanceList = repo.GetAttendanceListFor("CH5490");
+
+            // Assert
+            var barcodes = attendanceList.Select(attendance => attendance.Barcode).ToList();
+            CollectionAssert.AreEqual(barcodes, expectedBarcodes);
         }
     }
 }
--- InsiteTeamTask/Repositories/DataRepository.cs
+++ InsiteTeamTask/Repositories/DataRepository.cs
@@ -52,6 +52,83 @@ namespace InsiteTeamTask.Repositories
             return attendanceList;
         }
 
+        public List<Attendance> GetAttendanceListFor(int gameNumber, int seasonNumber)
+        {
+            MockDataService service = new MockDataService();
+
+            List<Member> members = service.Members().ToList();
+            List<Ticket> tickets = service.Tickets().ToList();
+            List<Product> products = service.Products().ToList();
+
+            List<Attendance> attendanceList = new List<Attendance>();
+
+            var filteredMembers = members.Where(member => {
+                var filteredProduct = products.Single(product => product.Id == member.ProductId);
+
+                return filteredProduct.GameId == gameNumber && filteredProduct.SeasonId == seasonNumber;
+            })
+                .Select(member =>
+                {
+                    return new Attendance
+                    {
+                        Barcode = "N/A",
+                        MemberId = member.Id
+                    };
+                });
+
+            attendanceList.AddRange(filteredMembers);
+
+            var filteredTickets = tickets.Where(ticket => {
+                var filteredProduct = products.Single(product => product.Id == ticket.ProductId);
+
+                return filteredProduct.GameId == gameNumber && filteredProduct.SeasonId == seasonNumber;
+            })
+                .Select(ticket =>
+                {
+                    return new Attendance
+                    {
+                        Barcode = ticket.Barcode,
+                        MemberId = 0
+                    };
+                });
+
+            attendanceList.AddRange(filteredTickets);
+
+            return attendanceList;
+        }
+
+        public List<Attendance> GetAttendanceListFor(string productCode)
+        {
+            MockDataService service = new MockDataService();
+
+            List<Member> members = service.Members().ToList();
+            List<Ticket> tickets = service.Tickets().ToList();
+
+            List<Attendance> attendanceList = new List<Attendance>();
+
+            var filteredMembers = members
+                .Where(member => member.ProductId == productCode)
+                .Select(member => new Attendance
+                {
+                    Barcode = "N/A",
+                    MemberId = member.Id
+                });
+
+            attendanceList.AddRange(filteredMembers);
+
+            var filteredTickets = tickets
+                .Where(ticket => ticket.ProductId == productCode)
+                .Select(ticket => new Attendance
+                {
+                    Barcode = ticket.Barcode,
+                    MemberId = 0
+                });
+
+            attendanceList.AddRange(filteredTickets);
+
+            return attendanceList;
+        }
+
         public List<Season> GetSeasons(int eventId)
         {
             throw new NotImplementedException();
